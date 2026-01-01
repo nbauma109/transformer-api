61fea780f43eaa57ad4afe8df91cc4181644307a
nbauma109 <nbauma109@github.com>
2026-01-01 21:14:03 +0100

    improve code coverage

---
+++ src/test/java/com/heliosdecompiler/transformerapi/ZipLoader.java
@@ -0,0 +1,28 @@
+package com.heliosdecompiler.transformerapi;
+
+import org.jd.core.v1.util.StringConstants;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ZipLoader extends org.jd.core.v1.util.ZipLoader {
+
+    public ZipLoader(InputStream in) throws IOException {
+        super(in);
+    }
+
+    @Override
+    protected String makeEntryName(String entryName) {
+        return entryName;
+    }
+
+    @Override
+    public byte[] load(String internalName) throws IOException {
+        return getMap().get(internalName + StringConstants.CLASS_FILE_SUFFIX);
+    }
+
+    @Override
+    public boolean canLoad(String internalName) {
+        return getMap().containsKey(internalName + StringConstants.CLASS_FILE_SUFFIX);
+    }
+}
