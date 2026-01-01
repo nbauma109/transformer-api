1fcff23bfdb1a25b5f7e12d7cd01a3f220fa2fc9
nbauma109 <nbauma109@users.noreply.github.com>
2026-01-01 21:19:44 +0100

    improve code coverage (#164)
    * improve code coverage
    
    * add test resource jar and update gitignore
    
    ---------
    
    Co-authored-by: nbauma109 <nbauma109@github.com>

--- src/test/java/com/heliosdecompiler/transformerapi/StandardTransformersTest.java
+++ src/test/java/com/heliosdecompiler/transformerapi/StandardTransformersTest.java
@@ -1,73 +1,74 @@
 package com.heliosdecompiler.transformerapi;
 
 import org.apache.commons.io.IOUtils;
-import org.jd.core.v1.loader.ClassPathLoader;
 import org.junit.Test;
 
 import com.heliosdecompiler.transformerapi.common.Loader;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Map;
 
-import jd.core.DecompilationResult;
-
-import static org.junit.Assert.assertEquals;
 import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_CFR;
-import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_PROCYON;
 import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_FERNFLOWER;
-import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_VINEFLOWER;
 import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_JADX;
 import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_JD_CORE_V0;
 import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_JD_CORE_V1;
-
-import static jd.core.preferences.Preferences.WRITE_METADATA;
+import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_PROCYON;
+import static com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers.ENGINE_VINEFLOWER;
 import static jd.core.preferences.Preferences.WRITE_LINE_NUMBERS;
+import static jd.core.preferences.Preferences.WRITE_METADATA;
+import static org.junit.Assert.assertEquals;
+
+import jd.core.DecompilationResult;
 
 public class StandardTransformersTest {
 
     @Test
     public void testDecompileCFR() throws Exception {
-        testDecompile("/HelloWorldCFR.txt", ENGINE_CFR, Map.of("showversion", "false"));
+        testDecompile("/TestCompactCFR.txt", ENGINE_CFR, Map.of("showversion", "false"));
     }
-
+    
     @Test
     public void testDecompileProcyon() throws Exception {
-        testDecompile("/HelloWorldProcyon.txt", ENGINE_PROCYON, Map.of("SuppressBanner", "true"));
+        testDecompile("/TestCompactProcyon.txt", ENGINE_PROCYON, Map.of("SuppressBanner", "true"));
     }
     
     @Test
     public void testDecompileJADX() throws Exception {
-        testDecompile("/HelloWorldJADX.txt", ENGINE_JADX, Collections.emptyMap());
+        testDecompile("/TestCompactJADX.txt", ENGINE_JADX, Collections.emptyMap());
     }
     
     @Test
     public void testDecompileFernflower() throws Exception {
-        testDecompile("/HelloWorldFernflower.txt", ENGINE_FERNFLOWER, Collections.emptyMap());
+        testDecompile("/TestCompactFernflower.txt", ENGINE_FERNFLOWER, Collections.emptyMap());
     }
     
     @Test
     public void testDecompileVineflower() throws Exception {
-        testDecompile("/HelloWorldVineflower.txt", ENGINE_VINEFLOWER, Collections.emptyMap());
+        testDecompile("/TestCompactVineflower.txt", ENGINE_VINEFLOWER, Collections.emptyMap());
     }
     
     @Test
     public void testDecompileJDCoreV0() throws Exception {
-        testDecompile("/HelloWorldJDCoreV0.txt", ENGINE_JD_CORE_V0, Collections.emptyMap());
+        testDecompile("/TestCompactJDCoreV0.txt", ENGINE_JD_CORE_V0, Collections.emptyMap());
     }
     
     @Test
     public void testDecompileJDCoreV1() throws Exception {
-        testDecompile("/HelloWorldJDCoreV1.txt", ENGINE_JD_CORE_V1, Map.of(WRITE_LINE_NUMBERS, "false", WRITE_METADATA, "false"));
+        testDecompile("/TestCompactJDCoreV1.txt", ENGINE_JD_CORE_V1, Map.of(WRITE_LINE_NUMBERS, "false", WRITE_METADATA, "false"));
     }
     
     private void testDecompile(String path, String engineName, Map<String, String> preferences)
-            throws TransformationException, IOException, IllegalAccessException, InvocationTargetException {
-        ClassPathLoader classPathLoader = new ClassPathLoader();
-        Loader loader = new Loader(classPathLoader::canLoad, classPathLoader::load);
-        String internalName = HelloWorld.class.getName().replace('.', '/');
+            throws TransformationException, IOException, IllegalAccessException, InvocationTargetException, URISyntaxException {
+        URI resource = getClass().getResource("/test-compact-expand-inline.jar").toURI();
+        ZipLoader zipLoader = new ZipLoader(resource.toURL().openStream());
+        Loader loader = new Loader(zipLoader::canLoad, zipLoader::load, resource);
+        String internalName = "test/TestCompact";
         DecompilationResult result = StandardTransformers.decompile(loader, internalName, preferences, engineName);
         assertEqualsIgnoreEOL(getResourceAsString(path), result.getDecompiledOutput());
     }
