d4f1612f54519cc1dd92ccbbb4fa2646cbee211a
nbauma109 <nbauma109@github.com>
2026-01-31 11:41:20 +0100

    Make a better use of JADX options

--- src/main/java/com/heliosdecompiler/transformerapi/decompilers/jadx/MapJadxArgs.java
+++ src/main/java/com/heliosdecompiler/transformerapi/decompilers/jadx/MapJadxArgs.java
@@ -3,22 +3,32 @@ package com.heliosdecompiler.transformerapi.decompilers.jadx;
 import com.heliosdecompiler.transformerapi.common.SettingsApplicable;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import jadx.api.JadxArgs;
 
 public class MapJadxArgs extends JadxArgs implements SettingsApplicable  {
 
+    private static final Map<String, String> COMMON_OPTIONS = Map.of(
+            "MoveInnerClasses", "true",
+            "InlineAnonymousClasses", "true",
+            "UseImports", "true"
+    );
+
     public MapJadxArgs(Map<String, String> settings) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         applySettings(settings);
     }
 
     public static Map<String, String> defaults() {
-        return Map.of("MoveInnerClasses", "true");
+        return COMMON_OPTIONS;
     }
 
     public static Map<String, String> lineNumbers() {
-        return Map.of("InsertDebugLines", "true", "DebugInfo", "true", "MoveInnerClasses", "true");
+        Map<String, String> options = new HashMap<>(COMMON_OPTIONS);
+        options.put("InsertDebugLines", "true");
+        options.put("DebugInfo", "true");
+        return Collections.unmodifiableMap(options);
     }
-
 }
