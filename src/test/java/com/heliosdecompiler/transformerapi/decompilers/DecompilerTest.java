df8a7f46941c067f9b271865bb48869f92333ebd
nbauma109 <nbauma109@github.com>
2026-02-11 13:47:04 +0100

    Remove bad tests

--- src/test/java/com/heliosdecompiler/transformerapi/decompilers/DecompilerTest.java
+++ src/test/java/com/heliosdecompiler/transformerapi/decompilers/DecompilerTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import com.heliosdecompiler.transformerapi.StandardTransformers.Decompilers;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -22,17 +21,6 @@ public class DecompilerTest {
         assertFalse(Decompilers.JADX.supportsRealignment());
     }
 
-    @Test
-    public void testGetDecompileTime() {
-        assertEquals(0, Decompilers.JD_CORE_V0.getDecompilationTime());
-        assertEquals(0, Decompilers.JD_CORE_V1.getDecompilationTime());
-        assertEquals(0, Decompilers.PROCYON.getDecompilationTime());
-        assertEquals(0, Decompilers.FERNFLOWER.getDecompilationTime());
-        assertEquals(0, Decompilers.VINEFLOWER.getDecompilationTime());
-        assertEquals(0, Decompilers.CFR.getDecompilationTime());
-        assertEquals(0, Decompilers.JADX.getDecompilationTime());
-    }
-    
     @Test
     public void testGetDecompilerVersion() {
         assertNotNull(Decompilers.PROCYON.getDecompilerVersion());
